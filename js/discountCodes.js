9338f2c7130d289474675abf28057421dc12670f
objectProfessionals <objectProfessionals@googlemail.com>
2016-02-23 11:13:03 +0000

    Add first 100 files

---
+++ js/discountCodes.js
@@ -0,0 +1,3 @@
+var discounts = {
+		'code1': {code:'aaaaa', pc: 20, exp:'01/01/2017'}
+};
---
+++ js/manipulateCart.js
@@ -0,0 +1,60 @@
+simpleCart({
+    checkout: {
+      type: "PayPal",
+      email: "emailSanj@yahoo.com"
+    }
+  });
+simpleCart({
+    currency: "GBP" // set the currency to pounds sterling
+});
+
+simpleCart.bind( "afterAdd" , function( item ){
+    updateCartAlert();
+    updateCartClass();
+});
+
+simpleCart.bind( "update" , function( item ){
+    updateCartClass();
+});
+
+simpleCart.ready( function(){
+	updateCartClass();
+});
+
+function updateCartAlert() {
+	var myCartAlert = document.getElementById('myCartAlert');
+	myCartAlert.className='alert alert-info';
+}
+
+function updateCartClass() {
+	var cartMenu = document.getElementById('cartMenu');
+	var cartIcon = document.getElementById('cartIcon');
+	if(simpleCart.quantity() == 0)
+	{
+		cartIcon.style.visibility='hidden';
+	} else {
+		cartMenu.className='active2';
+		cartIcon.style.visibility='';
+	}
+}
+
+function getDiscounted() {
+	var discountEl = document.getElementById('discount');
+	if (discountEl && discountEl.value) {
+		for (var d in discounts) {
+			var drow = discounts[d];
+			if (drow.code && drow.code==discountEl.value) {
+				var expiry = new Date(drow.exp);
+				var now = new Date();
+				if (now < expiry) {
+					alert('The discount code you entered has been applied');
+					return (1-(drow.pc /100));
+				} else {
+					alert('The discount code you entered was not valid');
+				}
+			}
+		}
+	}
+	
+	return 1;
+}
