7efe058de26309c3ebc1faec3e3f8fde3f79dde7
sParekh <sParekh@sParekh-HOME-PC.Home>
2016-03-07 10:46:35 +0000

    Clean up footer as controller

---
+++ app/shared/footer/footerController.js
@@ -0,0 +1,7 @@
+myMoviesApp.controller('footerCtrl', ['$scope', function($scope) {
+$scope.myFooter = '<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">'
+    				+ '<div align="center">'
+    					+ '<h3><a class="fa fa-facebook-square fa-2x" href="https://www.facebook.com/sanjay.parekh.1840"></a>&nbsp;<a class="fa fa-twitter-square fa-2x" href="https://twitter.com/readMyArt"></a> by <a href="https://www.readMyArt.com">&nbsp;readmyart.com</a></h3>'
+    				+ '</div>'
+    			+ '</nav>';
+}]);
--- js/footer.js
+++
@@ -1,5 +0,0 @@
-document.write('<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">');
-document.write('<div align="center">');
-document.write('<h3><a class="fa fa-facebook-square fa-2x" href="https://www.facebook.com/sanjay.parekh.1840"></a>&nbsp;<a class="fa fa-twitter-square fa-2x" href="https://twitter.com/readMyArt"></a> by <a href="https://www.readMyArt.com">&nbsp;readmyart.com</a></h3>');
-document.write('</div>');
-document.write('</nav>');
