aa7a56f745984c4e8f951c74f0b305c1dd0ed9d3
objectProfessionals <objectProfessionals@googlemail.com>
2017-07-01 18:43:31 +0100

    changes to footer

--- js/contact_me.php
+++ js/contact_me.php
@@ -13,7 +13,7 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'objectProfessionals@readMyArt.com'; // put your email
+$to = 'objectProfessionals@gmail.com'; // put your email
 $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
