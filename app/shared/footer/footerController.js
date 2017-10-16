2263979dc74447934eb351fbae616b582fa112d7
objectProfessionals <objectProfessionals@googlemail.com>
2017-10-16 11:59:50 +0100

    Change to LonGenArt

--- app/shared/footer/footerController.js
+++ app/shared/footer/footerController.js
@@ -1,7 +1,7 @@
 myMoviesApp.controller('footerCtrl', ['$scope', function($scope) {
 $scope.myFooter = '<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">'
     				+ '<div align="center">'
-    					+ '<h3><a class="fa fa-facebook-square fa-2x" href="https://www.facebook.com/sanjay.parekh.1840"></a>&nbsp;<a class="fa fa-instagram fa-2x" href="https://www.instagram.com/londongenerativeart"></a> by <a href="https://www.movie-maps.net">&nbsp;movie-maps.net</a></h3>'
+    					+ '<h3><a class="fa fa-facebook-square fa-2x" href="https://www.facebook.com/sanjay.parekh.1840"></a>&nbsp;<a class="fa fa-instagram fa-2x" href="https://www.instagram.com/londongenerativeart"></a> by <a href="https://www.lonGenArt.com">&nbsp;lonGenArt.com</a></h3>'
     				+ '</div>'
     			+ '</nav>';
 }]);
--- js/contact_me.js
+++ js/contact_me.js
@@ -45,7 +45,7 @@ $(function() {
  		 $('#success').html("<div class='alert alert-danger'>");
             	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:contact@movie-maps.net?Subject=Message_Me from movie-maps.net'>contact@movie-maps.net</a> ? Sorry for the inconvenience!");
+            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:objectProfessionals@gmail.com?Subject=Message_Me from lonGenArt.com'>objectProfessionals@gmail.com</a> ? Sorry for the inconvenience!");
  	        $('#success > .alert-danger').append('</div>');
  		//clear all fields
  		$('#contactForm').trigger("reset");
